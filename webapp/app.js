46c3cd866c8a5b8b762b4136b88bf3c626495e8e
Diogo Osório <diogo.g.osorio@gmail.com>
2014-02-01 15:10:30 +0000

    Tweaked the app bootstrap a bit.

--- webapp/app.js
+++ webapp/app.js
@@ -1,39 +1,46 @@
-
-/**
- * Module dependencies.
- */
-
+// modules
 var express = require('express');
+var app = express();
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var mongoose = require('mongoose');
 
-var app = express();
+// configuration files
+var db = require('./config/db');
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('e4933f5566c21bdca6b1077c6f60f1429d6966432be4def199d2a78991d2c800eecbe4bfd6044147c91c43406072035077656730ba476f7cb6285849fd2e45bd'));
-app.use(express.session());
-app.use(app.router);
-app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
-app.use(express.static(path.join(__dirname, 'public')));
+// configuration
+app.configure(function() {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('e4933f5566c21bdca6b1077c6f60f1429d6966432be4def199d2a78991d2c800eecbe4bfd6044147c91c43406072035077656730ba476f7cb6285849fd2e45bd'));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
+    app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
+    mongoose.connect(db.url);
+});
+
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// routes
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+// start app
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+// expose the app
+exports = module.exports = app; 
