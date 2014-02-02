46dcce5fc6e5844781433c182ed892efcf8d2293
Diogo Osório <diogo.g.osorio@gmail.com>
2014-02-02 12:36:29 +0000

    Removed old routing files.

--- webapp/routes/api.js
+++
@@ -1,7 +0,0 @@
-// api endpoints
-module.exports = function(app) {
-    app.get('/api/test', function(req, res) {
-        var response = {a: 'b', c: 'd'};
-        res.json(response);
-    });
-};
--- webapp/routes/frontend.js
+++ webapp/routes/frontend.js
@@ -1,6 +1,6 @@
 // Frontend routes
 module.exports = function(app) {
-    app.get('*', function(req, res) {
-        res.sendfile('./public/views/index.html');
+    app.get('/', function(req, res) {
+        res.render('index.html');
     });
 };
