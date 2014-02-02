35af2cd498341ec35f1b0f6ab2b49922fdcccfa3
Diogo Osório <diogo.g.osorio@gmail.com>
2014-02-02 12:35:43 +0000

    Added nunjucks as the template engine.

--- webapp/app.js
+++ webapp/app.js
@@ -4,29 +4,33 @@ var app = express();
 var http = require('http');
 var path = require('path');
 var mongoose = require('mongoose');
+var nunjucks = require('nunjucks');
 
 // configuration files
 var db = require('./config/db');
 mongoose.connect(db.url);
 
 // configuration
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'nunjucks');
-app.use("/public", express.static(path.join(__dirname, 'public')));
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.methodOverride());
-app.use(express.cookieParser('e4933f5566c21bdca6b1077c6f60f1429d6966432be4def199d2a78991d2c800eecbe4bfd6044147c91c43406072035077656730ba476f7cb6285849fd2e45bd'));
-app.use(express.session());
-app.use(app.router);
+app.configure(function() {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', path.join(__dirname, 'views'));
 
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+    nunjucks.configure('views', {
+        autoescape: true,
+        express: app
+    });
+
+    app.use("/public", express.static(path.join(__dirname, 'public')));
+    app.use(express.logger('dev'));
+    app.use(express.json());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('e4933f5566c21bdca6b1077c6f60f1429d6966432be4def199d2a78991d2c800eecbe4bfd6044147c91c43406072035077656730ba476f7cb6285849fd2e45bd'));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(express.errorHandler());
+});
 
 // routes
-require('./routes/api.js')(app);
 require('./routes/frontend.js')(app);
 
 // start app
