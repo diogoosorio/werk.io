c0ad1dcb11451a310c1621b138158663dce0fe3a
Diogo Osório <diogo.g.osorio@gmail.com>
2014-02-04 23:33:58 +0000

    Pass the technologies / consultancies list to the scrapper.

--- importer/src/feeder.py
+++ importer/src/feeder.py
@@ -2,38 +2,52 @@
 
 import scrappers
 import pymongo
+import os
+import sys
+import datetime
+import re
 
 from config import Config
 
-SCRAPE_LIMIT = 400
 SCRAPPERS = (
     ('itjobs', scrappers.ITJobs),
 )
 
 class JobFeeder(object):
+
     def __init__(self, scrappers):
         self.scrappers = scrappers
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
 
     def bootstrap(self):
         mongostr = "mongodb://%s:%s" % (Config.MONGO_HOST, Config.MONGO_PORT)
         mongo = pymongo.MongoClient(mongostr)
         self.db = mongo[Config.MONGO_DB]
+        self.load_configuration()
+
+    def load_configuration(self):
+        technologies_file  = os.path.join(self.current_dir, 'config', 'technologies.txt')
+        consultancies_file = os.path.join(self.current_dir, 'config', 'consultancies.txt')
+
+        self.technologies  = open(technologies_file).read().strip().split('\n')
+        self.consultancies = open(consultancies_file).read().strip().split('\n')
+
 
     def run(self):
         for source, scrapper_class in self.scrappers:
-            scrapper   = scrapper_class(self.db)
+            scrapper   = scrapper_class(self.db, self.consultancies, self.technologies)
             doc_cursor = self.db.jobs.find({'source': source})
             doc_cursor.sort('publish_date')
             doc_cursor.limit(1)
 
             if doc_cursor.count() > 0:
                 document = doc_cursor[0]
-                nentries = None
+                date = None
             else:
                 document = None
-                nentries = SCRAPE_LIMIT
+                date = datetime.datetime.now() - datetime.timedelta(days=90)
 
-            scrapper.scrape(document, nentries)
+            scrapper.scrape(document, date)
 
 
 if __name__ == '__main__':
