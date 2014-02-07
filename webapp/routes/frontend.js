125578f60274b05a39fe41995dd25dceeb8e43d0
Diogo Osório <diogo.g.osorio@gmail.com>
2014-02-07 00:49:45 +0000

    Pass the jobs to the tempalte.

--- webapp/routes/frontend.js
+++ webapp/routes/frontend.js
@@ -1,6 +1,35 @@
 // Frontend routes
+
+var models   = require('../lib/models');
+var pagesize = 10;
+
 module.exports = function(app) {
+
     app.get('/', function(req, res) {
-        res.render('index.html');
+        var page = req.query.page !== undefined ? parseInt(req.query.page) : 1,
+            render = function(err, jobs) {
+                var ordered_jobs = {},
+                    date, job, key;
+
+                for (key in jobs) {
+                    job  = jobs[key];
+                    date = job.publish_date;
+
+                    if (ordered_jobs[date.getTime()] === undefined) {
+                        ordered_jobs[date.getTime()] = {date: date, entries: []};
+                    }
+
+                    ordered_jobs[date.getTime()].entries.push(job);
+                }
+
+                res.render('index.html', {jobs: ordered_jobs});
+            };
+
+        models.Job.find()
+            .limit(pagesize)
+            .skip((page - 1) * pagesize)
+            .sort('-publish_date')
+            .exec(render);
     });
+
 };
