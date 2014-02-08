6b5441c17a287d69cf2298ee0f6773f0dc486812
Diogo Osório <diogo.g.osorio@gmail.com>
2014-02-08 13:08:44 +0000

    Addded the total jobs to the template.

--- webapp/routes/frontend.js
+++ webapp/routes/frontend.js
@@ -7,9 +7,9 @@ module.exports = function(app) {
 
     app.get('/', function(req, res) {
         var page = req.query.page !== undefined ? parseInt(req.query.page) : 1,
-            render = function(err, jobs) {
+            render = function(err, jobs, count) {
                 var ordered_jobs = {},
-                    date, job, key;
+                date, job, key;
 
                 for (key in jobs) {
                     job  = jobs[key];
@@ -22,14 +22,19 @@ module.exports = function(app) {
                     ordered_jobs[date.getTime()].entries.push(job);
                 }
 
-                res.render('index.html', {jobs: ordered_jobs});
+                res.render('index.html', {jobs: ordered_jobs, page: page, pages: Math.ceil(count / page)});
             };
 
         models.Job.find({is_consultancy: false})
             .limit(pagesize)
             .skip((page - 1) * pagesize)
             .sort('-publish_date')
-            .exec(render);
+            .exec(function(err, jobs) {
+                models.Job.count({is_consultancy: false})
+                    .exec(function(err, count) {
+                        return render(err, jobs, count);
+                    });
+             });
     });
 
 
